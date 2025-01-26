7fa1027f1265b755bf156d82a9be24871ee84382
aapolaivm <aapo.laivamaa@gmail.com>
2025-01-26 14:22:06 +0200

    Exercise 1.05 ja aijemmat yaml tiedostojen lisäykset

--- Project/server.py
+++ Project/server.py
@@ -5,7 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Todo app server is running!"
+    return """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Todo App</title>
+    </head>
+    <body>
+        <h1>Welcome to the Todo App!</h1>
+        <p>This is a simple HTML response served by Flask.</p>
+    </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     # Getting port value
